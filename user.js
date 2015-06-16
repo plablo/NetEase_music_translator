ad10adea606dda1e23731a639e02e6c3a0d0fcd8
plablo <insprp00@gmail.com>
2015-06-16 15:35:32 +0200

    Minor changes
    * Changed comments
    * Changed some indexation
    * Changed version to 0.4

--- user.js
+++ user.js
@@ -2,8 +2,8 @@
 // @id           NetEase_Music_Cloud_Translator
 // @name         NetEase Music Cloud Translator
 // @namespace    https://github.com/plablo/NetEase_music_translator
-// @version      0.3
-// @description  Translate from the chinese to your language at music.163.com
+// @version      0.4
+// @description  Translate from chinese to your language at music.163.com
 // @author       Plablo
 // @include      http://music.163.com/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
@@ -32,25 +32,25 @@ container_div.appendChild(google_tt_div);
 var google_tt_script = document.createElement('script'); //create a script tag
 google_tt_script.type = 'text/javascript'; // add type attribute
 google_tt_script.innerHTML = 'function googleTranslateElementInit() {new google.translate.TranslateElement({pageLanguage: \'zh-CN\', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false, multilanguagePage: true}, \'google_translate_element\');}';
-container_div.appendChild(google_tt_script); // Insert it as the last child of body
+container_div.appendChild(google_tt_script); // Insert it as the last child of container_div
 
 var google_tt_script_link = document.createElement('script'); //create a script tag
 google_tt_script_link.type = 'text/javascript'; // add type attribute
 google_tt_script_link.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-container_div.appendChild(google_tt_script_link); // Insert it as the last child of body
+container_div.appendChild(google_tt_script_link); // Insert it as the last child of container_div
 
 //Append to the end of the main body
 $('body').filter(function(){return this.id.match(/auto-id-./);}).get(0).appendChild(container_div);
 
-//Hide the Google Translator Bar
-var node = document.createElement("style"); //iframe.goog-te-banner-frame.skiptranslate {display: none !important;} .skiptranslate {display: none !important;}
+//Hide the Google Translator Bar using CSS
+var node = document.createElement("style");
 var css = "iframe.goog-te-banner-frame {display: none !important;} #goog-gt-tt {display: none !important;} .goog-te-gadget {display: block !important;} body {top: 0px !important;}" 
-	node.type = "text/css";
-	node.appendChild(document.createTextNode(css));
-	var heads = document.getElementsByTagName("head");
-	if (heads.length > 0) {
-		heads[0].appendChild(node); 
-	} else {
-		// no head yet, stick it whereever
-		document.documentElement.appendChild(node);
-	}
+node.type = "text/css";
+node.appendChild(document.createTextNode(css));
+var heads = document.getElementsByTagName("head");
+if (heads.length > 0) {
+    heads[0].appendChild(node); 
+} else {
+    // no head yet, stick it whereever
+    document.documentElement.appendChild(node);
+}
