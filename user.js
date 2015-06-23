5c8565cf134275f6533fef5fa10916fb398969ab
plablo <insprp00@gmail.com>
2015-06-23 09:02:07 +0200

    Minor changes
    * Added license metatag
    * Changed version to 0.4.1

--- user.js
+++ user.js
@@ -2,9 +2,10 @@
 // @id           NetEase_Music_Cloud_Translator
 // @name         NetEase Music Cloud Translator
 // @namespace    https://github.com/plablo/NetEase_music_translator
-// @version      0.4
+// @version      0.4.1
 // @description  Translate from chinese to your language at music.163.com
 // @author       Plablo
+// @license      NetEase Music Cloud Translator is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
 // @include      http://music.163.com/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @updateURL    https://raw.githubusercontent.com/plablo/NetEase_music_translator/master/user.js
